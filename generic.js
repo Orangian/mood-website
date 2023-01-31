23be5a2bab968b180d2dfa359028204ad7da1e58
Orangian <tysayahi@gmail.com>
2023-01-31 13:38:07 -0500

    Swatch

--- generic.js
+++ generic.js
@@ -9,11 +9,21 @@ function hide() {
     document.getElementById(dropdown).style.visibility=hidden;
 }
 
+var current = document.getElementById("headlogo").src;
 
 function swatch() {
-if (document.getElementById("headlogo").src == "header-logo-dash.svg"){
+if (current.includes("header-logo-dash.svg") === true){
     document.getElementById("headlogo").src = "header-logo-nodash.svg";
+    current = document.getElementById("headlogo").src;
 } else {
     document.getElementById("headlogo").src = "header-logo-dash.svg";
+    current = document.getElementById("headlogo").src;
 }
 }
+
+
+
+const string = "foo";
+const substring = "oo";
+
+console.log(string.includes(substring)); // true
