31b4dcbf53f150cea8a906dabfaf1b7cc951ac2e
Orangian <tysayahi@gmail.com>
2023-02-15 09:22:21 -0500

    Add archaic, new logo

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-var dropdown = "dropdown";
-var shown = "visible";
-var hidden = "hidden";
-function show() {
-    document.getElementById(dropdown).style.visibility=shown;
-}
+// var current;
 
-function hide() {
-    document.getElementById(dropdown).style.visibility=hidden;
-}
-
-var current;
-
-function swatch() {
-    current = document.getElementById("headlogo").src;
-if (current.includes("header-logo-dash.svg") === true){
-    document.getElementById("headlogo").src = "header-logo-nodash.svg";
-    current = document.getElementById("headlogo").src;
-} else {
-    document.getElementById("headlogo").src = "header-logo-dash.svg";
-    current = document.getElementById("headlogo").src;
-}
-}
+// function swatch() {
+//     current = document.getElementById("headlogo").src;
+// if (current.includes("header-logo-dash.svg") === true){
+//     document.getElementById("headlogo").src = "header-logo-nodash.svg";
+//     current = document.getElementById("headlogo").src;
+// } else {
+//     document.getElementById("headlogo").src = "header-logo-dash.svg";
+//     current = document.getElementById("headlogo").src;
+// }
+// }
 
