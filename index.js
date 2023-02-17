c338a1200ad020857dd61f3c1a12638ac9b31965
Orangian <tysayahi@gmail.com>
2023-02-17 11:49:22 -0500

    Modify index.{css,html,js}

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// var current;
+var current;
 
-// function swatch() {
-//     current = document.getElementById("headlogo").src;
-// if (current.includes("header-logo-dash.svg") === true){
-//     document.getElementById("headlogo").src = "header-logo-nodash.svg";
-//     current = document.getElementById("headlogo").src;
-// } else {
-//     document.getElementById("headlogo").src = "header-logo-dash.svg";
-//     current = document.getElementById("headlogo").src;
-// }
-// }
+function swatch() {
+    var element = document.getElementById("headlogo");
+    var current = getComputedStyle(element, "").getPropertyValue('--head');
+    
+    if(document.getElementById("formal").checked) {
+        formalswatch();
+    } else if (document.getElementById("informal").checked) {
+        true
+    } else if (document.getElementById("archaic").checked)
+        archaicswatch();
+}
 
+function formalswatch() {
+    var element = document.getElementById("headlogo");
+    var current = getComputedStyle(element, "").getPropertyValue('--head');
+    if (current.includes("header-logo-nodash") === true) {
+    element.style.setProperty('--head', "url(https://raw.githubusercontent.com/Orangian/mood-website/master/header-logo-dash.svg)");
+}   else if (current.includes("header-logo-dash") === true) {
+    element.style.setProperty('--head', "url(https://raw.githubusercontent.com/Orangian/mood-website/master/header-logo-nodash.svg)");
+}
+}
+
+function archaicswatch() {
+    var element = document.getElementById("headlogo");
+    var current = getComputedStyle(element, "").getPropertyValue('--head');
+    if (current.includes("undashed-sketched") === true) {
+    element.style.setProperty('--head', "url(https://raw.githubusercontent.com/Orangian/mood-website/master/logo-dashed-sketched-black-text.svg)");
+}   else if (current.includes("dashed-sketched") === true) {
+    element.style.setProperty('--head', "url(https://raw.githubusercontent.com/Orangian/mood-website/master/logo-undashed-sketched-black-text.svg)");
+}
+}
+
+function formaldefault() {
+    
+}
